55e40f6602c7a73cf88a07898cc05f4abd143323
Daniel Adeniji <daniel_adeniji@hotmail.com>
2024-04-14 10:56:49 -0700

    Update property.dadeniji.validation.ps1

--- validation/dadeniji/property.dadeniji.validation.ps1
+++ validation/dadeniji/property.dadeniji.validation.ps1
@@ -14,22 +14,135 @@ $skipPropertyCheck = $true;
     https://stackoverflow.com/questions/26997511/how-can-you-test-if-an-object-has-a-specific-property
     Answered by dan-gph
 #>
-function hasPropertyIn($object, $propertyName)
+
+function hasPropertyIn([object] $object, [string] $property )
 {
-    $hasProperty = $propertyName -in $object.PSobject.Properties.Name;
-    
+	
+	<#
+		Declare Variables
+	#>
+	[Boolean] $hasProperty = $false;
+	[string]  $functionName = "";
+	[string]  $log = "";
+	
+	<#
+		Get Function Name
+	#>
+	if ( $null -ne $MyInvocation )
+	{
+		
+		if ( $null -ne $MyInvocation.MyCommand )
+		{
+	
+			$functionName = $MyInvocation.MyCommand
+			
+		}
+		
+	}
+	
+	<#
+		$object Nullability Check
+	#>
+	if ($null -eq $object )
+	{
+			
+		$log = "In function {0}, `$object is null" -f $functionName;
+		
+		throw $log;
+		
+		return $bHasProperty;
+		
+	}
+
+	<#
+		$property Nullability Check
+	#>	
+	if ($null -eq $property )
+	{
+			
+		$log = "In function {0}, `$property is null" -f $property;
+		
+		throw $log;
+		
+		return $bHasProperty;
+		
+	}
+
+	<#
+		Is property in object.PSObject.Properties.Name list
+	#>
+    $hasProperty = $property -in $object.PSobject.Properties.Name;
+
+	<#
+		Return HasProperty
+	#>
     return($hasProperty);
+
+	
 }
 
 function hasPropertyMatch($object, $propertyName)
 {
-    $hasProperty = ( $object.psobject.properties.match($propertyName) )
+
+	<#
+		Declare Variables
+	#>
+	[Boolean] $hasProperty = $false;
+	[string]  $functionName = "";
+	[string]  $log = "";
+	
+	<#
+		Get Function Name
+	#>
+	if ( $null -ne $MyInvocation )
+	{
+		
+		if ( $null -ne $MyInvocation.MyCommand )
+		{
+	
+			$functionName = $MyInvocation.MyCommand
+			
+		}
+		
+	}
+	
+	<#
+		$object Nullability Check
+	#>
+	if ($null -eq $object )
+	{
+			
+		$log = "In function {0}, `$object is null" -f $functionName;
+		
+		throw $log;
+		
+		return $bHasProperty;
+		
+	}
+
+	<#
+		$property Nullability Check
+	#>	
+	if ($null -eq $property )
+	{
+			
+		$log = "In function {0}, `$property is null" -f $property;
+		
+		throw $log;
+		
+		return $bHasProperty;
+		
+	}
+
+
+        $hasProperty = ( $object.psobject.properties.match($propertyName) )
     
-    return($hasProperty);
+        return($hasProperty);
+	
 }
 
 <#
-	Instanciate a few objects
+	Instantiate a few objects
 #>
 $objHuman1 = [pscustomobject]@{
     name='L.L. Cool J';
